37232096a47e5ecc5d11efc32c9f979127464af8
Rahul Saha <saharahul039@gmail.com>
2018-12-15 16:43:04 +0600

    Add files via upload

---
+++ LinkList/Node.py
@@ -0,0 +1,14 @@
+'''
+    creating Node object
+    property------| data, next_node
+
+'''
+
+class Node(object):
+
+    # constructor
+    def __init__(self, data):
+        self.data = data;
+        self.next_node = None;
+
+
